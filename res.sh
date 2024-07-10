950914a1bbbbae56b161f687a2da36939cdfc17f
Shaho <shahooak@gmail.com>
2024-07-10 20:02:54 +0330

    Create res.sh

---
+++ res.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+clear
+
+ cat << "EOF"
+===============================================================================================
+==  __       ___      .___  ___.      _______. __    __       ___       __    __    ______   ==
+== |  |     /   \     |   \/   |     /       ||  |  |  |     /   \     |  |  |  |  /  __  \  ==
+== |  |    /  ^  \    |  \  /  |    |   (----`|  |__|  |    /  ^  \    |  |__|  | |  |  |  | ==
+== |  |   /  /_\  \   |  |\/|  |     \   \    |   __   |   /  /_\  \   |   __   | |  |  |  | ==
+== |  |  /  _____  \  |  |  |  | .----)   |   |  |  |  |  /  _____  \  |  |  |  | |  `--'  | ==
+== |__| /__/     \__\ |__|  |__| |_______/    |__|  |__| /__/     \__\ |__|  |__|  \______/  ==
+==                                                                                           ==
+===============================================================================================
+             Developed by: @iamshaho
+             Twitter: https://twitter.com/iamshaho
+             Telegram: https://t.me/ubuntufornodes
+=========================================================================
+EOF
+
+read -p "1: check logs
+2: restart stationd and show logs
+3: rollback last 3 pods and show logs
+4: exit 
+" choice
+
+if [ "$choice" == "1" ]; then
+  sudo journalctl -u stationd -f --no-hostname -o cat
+elif [ "$choice" == "2" ]; then
+  echo "wait please ..."
+  sudo systemctl restart stationd && sudo journalctl -u stationd -f --no-hostname -o cat
+elif [ "$choice" == "3" ]; then
+  echo "wait please ..."
+  sudo systemctl stop stationd &&
+  cd tracks &&
+  git pull &&
+  go run cmd/main.go rollback &&
+  go run cmd/main.go rollback &&
+  go run cmd/main.go rollback &&
+  sudo systemctl restart stationd &&
+  sudo journalctl -u stationd -f --no-hostname -o cat
+elif [ "$choice" == "4" ]; then
+  exit
+else
+  echo "Not a valid choice"
+  exit
+fi
